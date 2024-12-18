e4973ee84abfefd396f8755793006f292caabe18
YashHaritash <yashharitash4@gmail.com>
2024-12-18 16:04:44 +0530

    setting up react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,60 @@
-import React, { Component } from 'react'
-import Navbar from './components/Navbar'
-import News from './components/News'
-
-// const API = "https://newsapi.org/v2/top-headlines?country=us&apiKey=57f1029c0d784b5f82b7b64937b17043"
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize={5} country="us" category="sports"/>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              key="home"
+              path="/"
+              element={<News pageSize={5} country="us" category="general" />}
+            />
+            <Route
+              key="general"
+              path="/general"
+              element={<News pageSize={5} country="us" category="general" />}
+            />
+            <Route
+              key="business"
+              path="/business"
+              element={<News pageSize={5} country="us" category="business" />}
+            />
+            <Route
+              key="entertainment"
+              path="/entertainment"
+              element={
+                <News pageSize={5} country="us" category="entertainment" />
+              }
+            />
+            <Route
+              key="health"
+              path="/health"
+              element={<News pageSize={5} country="us" category="health" />}
+            />
+            <Route
+              key="science"
+              path="/science"
+              element={<News pageSize={5} country="us" category="science" />}
+            />
+            <Route
+              key="sports"
+              path="/sports"
+              element={<News pageSize={5} country="us" category="sports" />}
+            />
+            <Route
+              key="technology"
+              path="/technology"
+              element={<News pageSize={5} country="us" category="technology" />}
+            />
+          </Routes>
+        </BrowserRouter>
       </div>
-    )
+    );
   }
 }
